ab62a2a9f7610485ec4318b4091c37639d048fb9
Krishmi <kuhkd150@gmail.com>
2025-06-20 21:00:07 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 
 // Load env vars
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: './.env' });
 
 // Connect to database
 connectDB();
