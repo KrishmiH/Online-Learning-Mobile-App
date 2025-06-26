e36168edc49aca6cc06bb00188b176f41801ff7a
Krishmi <kuhkd150@gmail.com>
2025-06-26 11:09:29 +0530

    App.js

--- LearningApp/App.js
+++ LearningApp/App.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { StatusBar } from 'expo-status-bar';
-import { AuthProvider } from './src/context/AuthContext';
-import AppNavigator from './src/navigation/AppNavigator';
-import { StyleSheet, View } from 'react-native';
+// import React from 'react';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { StatusBar } from 'expo-status-bar';
+// import { AuthProvider } from './src/context/AuthContext';
+// import AppNavigator from './src/navigation/AppNavigator';
+// import { StyleSheet, View } from 'react-native';
 
-export default function App() {
-  return (
-    <AuthProvider>
-      <NavigationContainer>
-        <View style={styles.container}>
-          <StatusBar style="light" />
-          <AppNavigator />
-        </View>
-      </NavigationContainer>
-    </AuthProvider>
-  );
-}
+// export default function App() {
+//   return (
+//     <AuthProvider>
+//       <NavigationContainer>
+//         <View style={styles.container}>
+//           <StatusBar style="light" />
+//           <AppNavigator />
+//         </View>
+//       </NavigationContainer>
+//     </AuthProvider>
+//   );
+// }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//   },
+// });
+
+// import AsyncStorage from '@react-native-async-storage/async-storage';
+// AsyncStorage.clear();
